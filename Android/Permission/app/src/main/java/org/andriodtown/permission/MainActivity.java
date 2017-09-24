21670c775476a3aa13e5f50d67c7561eec8cb606
unknown <kps990515@gmail.com>
2017-09-24 14:56:11 +0900

    revised

--- Android/Permission/app/src/main/java/org/andriodtown/permission/MainActivity.java
+++ Android/Permission/app/src/main/java/org/andriodtown/permission/MainActivity.java
@@ -1,13 +1,6 @@
 package org.andriodtown.permission;
 
-import android.os.Bundle;
-
 public class MainActivity extends BaseActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     public void init() {
