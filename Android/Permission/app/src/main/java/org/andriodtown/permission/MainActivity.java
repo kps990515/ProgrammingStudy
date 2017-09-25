49c7a9b2584f7e9ee562059866f32c330a12707c
unknown <kps990515@gmail.com>
2017-09-25 14:54:24 +0900

    revised

--- Android/Permission/app/src/main/java/org/andriodtown/permission/MainActivity.java
+++ Android/Permission/app/src/main/java/org/andriodtown/permission/MainActivity.java
@@ -7,12 +7,11 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 
     @Override
     public void init() {
-
+        setContentView(R.layout.activity_main);
     }
 
 }
