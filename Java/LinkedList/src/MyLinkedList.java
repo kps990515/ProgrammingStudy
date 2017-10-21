e4ec9599a0c8d297101e7921fef7276574bf5bd8
unknown <kps990515@gmail.com>
2017-10-21 10:43:28 +0900

    10/21

---
+++ Java/LinkedList/src/MyLinkedList.java
@@ -0,0 +1,29 @@
+
+public class MyLinkedList {
+	private Node header;
+	private int size;
+	private Node tail;
+	
+	public MyLinkedList() {
+		header = new Node(null);
+		size=0;
+	}
+	
+	private class Node{
+		private Object data;
+		private Node nextNode;
+		
+		Node(Object data){
+			this.data = data;
+			this.nextNode = null;
+		}
+	}
+	
+	public void addFirst(Object data) {
+		Node newNode = new Node(data);
+		newNode.nextNode = header.nextNode;
+		header.nextNode = newNode;
+		size++;
+	}
+	
+}
